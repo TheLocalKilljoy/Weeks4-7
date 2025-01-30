12c0389ee6169299eb8a2d4b95967a8657bff947
Chase Ross <chaseross002@gmail.com>
2025-01-30 10:48:47 -0500

    made some noise

--- Assets/Scripts/OnOff.cs
+++ Assets/Scripts/OnOff.cs
@@ -7,6 +7,8 @@ public class OnOff : MonoBehaviour
     public SpriteRenderer sr;
     public OnOff script;
     public GameObject go;
+    public AudioSource aud;
+    public AudioClip clip;
 
     void Update()
     {
@@ -14,14 +16,21 @@ public class OnOff : MonoBehaviour
         {
             //sr.enabled = false;
             //script.enabled = false;
-            go.SetActive(false);
+            //go.SetActive(false);
         }
         if (Input.GetKeyDown(KeyCode.Space))
         {
             //sr.enabled = true;
             //script.enabled = true;
-            go.SetActive(true);
+            //go.SetActive(true);
         }
 
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (aud.isPlaying == false)
+            {
+                aud.PlayOneShot(clip);
+            }
+        }
     }
 }
